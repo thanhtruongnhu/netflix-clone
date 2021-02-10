43176b6e8eb2f782426bb4d72dc05c47c161477a
truong.nhu <35893692+thanhtruongnhu@users.noreply.github.com>
2021-02-10 09:03:59 -0400

    Set up repo

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDgG_eaBB9f0cPrKSCcSIeQd4niDM1Rdb8",
+    authDomain: "netflix-clone-3b80f.firebaseapp.com",
+    projectId: "netflix-clone-3b80f",
+    storageBucket: "netflix-clone-3b80f.appspot.com",
+    messagingSenderId: "989895179142",
+    appId: "1:989895179142:web:e544c9653f74433c968a07"
+  };
