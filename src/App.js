34751b764fc140e0abae5ce5738c10f729f4e0e6
truong.nhu <35893692+thanhtruongnhu@users.noreply.github.com>
2021-02-11 13:21:25 -0400

    eslint setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Lets build netflix</h1>
     </div>
   );
 }
 
-export default App;
+export default App;    
+ 
