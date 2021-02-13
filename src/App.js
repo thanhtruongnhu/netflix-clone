60dc7321bee96dfd7af76a17d67d9c07eb7728fa
truong.nhu <35893692+thanhtruongnhu@users.noreply.github.com>
2021-02-13 16:08:48 -0400

    nav init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
+import HomeScreen from './HomeScreen';
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Lets build netflix</h1>
-    </div>
-  );
+	return (
+		<div className="app">
+			<HomeScreen />
+		</div>
+	);
 }
 
-export default App;    
- 
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav() {
+	return (
+		<div className="nav">
+			<img
+				src="https://rdmagazine.files.wordpress.com/2018/10/netflix-logo.jpg"
+				alt=""
+			/>
+			{/* Avatar */}
+			<img
+				src="https://live.staticflickr.com/65535/50916903991_32e09d803c_z.jpg"
+				alt=""
+			/>
+			<h1>This is the nav </h1>
+		</div>
+	);
+}
+
+export default Nav;
