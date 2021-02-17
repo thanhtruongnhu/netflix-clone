3a9e840c277a1cfa7a68116c0b1ab9b4fbb689b1
truong.nhu <35893692+thanhtruongnhu@users.noreply.github.com>
2021-02-17 09:39:18 -0400

    Redux 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
+import { logout, login } from './features/userSlice';
+import { useDispatch } from "react-redux"
 
 function App() {
 	const user = null;
+
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		/*this is a state Listener. also firebase save the data into the local cookies for your next time sign in */
+		auth.onAuthStateChanged((userAuth) => {
+			if (userAuth) {			
+				dispatch(login({
+					uid: userAuth.uid,
+					email: userAuth.email
+				}))
+			} else {
+				dispatch(logout)
+			}
+		});
+	}, []);
+
 	return (
 		<div className="app">
 			<Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,17 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDgG_eaBB9f0cPrKSCcSIeQd4niDM1Rdb8",
-    authDomain: "netflix-clone-3b80f.firebaseapp.com",
-    projectId: "netflix-clone-3b80f",
-    storageBucket: "netflix-clone-3b80f.appspot.com",
-    messagingSenderId: "989895179142",
-    appId: "1:989895179142:web:e544c9653f74433c968a07"
-  };
+	apiKey: 'AIzaSyDgG_eaBB9f0cPrKSCcSIeQd4niDM1Rdb8',
+	authDomain: 'netflix-clone-3b80f.firebaseapp.com',
+	projectId: 'netflix-clone-3b80f',
+	storageBucket: 'netflix-clone-3b80f.appspot.com',
+	messagingSenderId: '989895179142',
+	appId: '1:989895179142:web:e544c9653f74433c968a07',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
